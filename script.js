2899ac73f12647ac93d403a57ff26997026a502f
JK-B0T <103139241+JK-B0T@users.noreply.github.com>
2024-05-06 22:37:33 +0200

    Add board function

--- script.js
+++ script.js
@@ -2,5 +2,36 @@ document.addEventListener("DOMContentLoaded", main, false);
 
 function main() {
 
-    
+    const boardGrid = (() => {
+        const ROWS = 3;
+        const COLUMNS = 3;
+        let board = [];
+
+        for (let i = 0; i < COLUMNS; i++) {
+            board[i] = [];
+            for (let j = 0; j < ROWS; j++) {
+                board[i][j] = `${i}-${j}`;
+            }
+        }
+
+        const showGrid = () => {
+            let gridVisual = ""
+            for (let i = 0; i < COLUMNS; i++) {
+                gridVisual += `\n`;
+                for (let j = 0; j < ROWS; j++) {
+                    gridVisual += `${i}-${j} `;
+                }
+            }
+            return gridVisual;
+        }
+        return {board, showGrid};
+    })();
+
+    const newGrid = boardGrid;
+    console.log(newGrid.showGrid());
+    console.log("main ends");
+
+    function Cell () {
+        this.d
+    }
 }
