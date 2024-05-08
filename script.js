574256a5b0ca45f04482b592c948666f0cfa5d6b
JK-B0T <JK-B0T@users.noreply.github.com>
2024-05-08 14:01:00 +0200

    Add temporal classes and functions

--- script.js
+++ script.js
@@ -2,36 +2,97 @@ document.addEventListener("DOMContentLoaded", main, false);
 
 function main() {
 
-    const boardGrid = (() => {
-        const ROWS = 3;
-        const COLUMNS = 3;
-        let board = [];
+    function createCell (symbol = "") {
 
-        for (let i = 0; i < COLUMNS; i++) {
-            board[i] = [];
-            for (let j = 0; j < ROWS; j++) {
-                board[i][j] = `${i}-${j}`;
+        const setSymbol = (newsymbol) => {
+            symbol = newsymbol;
+        }
+
+        const getSymbol = () => {
+            return symbol;
+        }
+
+        return {setSymbol, getSymbol};
+    }
+
+    function createPlayer (name = "anon", symbol = "-") {
+        score = 0;
+
+        const getSymbol = () => {
+            return symbol;
+        }
+
+        const increaseScore = () => {
+            score++;
+        }
+
+        return {name, getSymbol, increaseScore};
+    }
+
+    const boardGrid = (function (rows, columns) {
+        let grid = [];
+
+        for (let i = 0; i < columns; i++) {
+            grid[i] = [];
+            for (let j = 0; j < rows; j++) {
+                grid[i][j] = createCell("");
             }
         }
 
         const showGrid = () => {
             let gridVisual = ""
-            for (let i = 0; i < COLUMNS; i++) {
-                gridVisual += `\n`;
-                for (let j = 0; j < ROWS; j++) {
-                    gridVisual += `${i}-${j} `;
+            for (let i = 0; i < columns; i++) {
+                for (let j = 0; j < rows; j++) {
+                    gridVisual += `[${grid[i][j].getSymbol()}] `;
                 }
+                gridVisual += `\n`;
             }
             return gridVisual;
         }
-        return {board, showGrid};
-    })();
 
-    const newGrid = boardGrid;
-    console.log(newGrid.showGrid());
+        return {grid, showGrid};
+    })(3, 3);
+
+    const gameController = (function (board) {
+        let player = {};
+
+        const checkPosition = (x, y) => {
+            board.grid[x][y].getSymbol() == "" ? true : false;
+        }
+
+        const setPlayer = (newPlayer) => {
+            player = newPlayer;
+        }
+
+        const setMove = (x, y) => {
+            console.log(x, y)
+            if (checkPosition(x, y)) {
+                board.grid[x][y].setSymbol(player.getSymbol());
+                console.log(x, y, board.grid[x][y].getSymbol(), player.getSymbol(), player)
+            } else {
+                return false;
+            }1
+        }
+
+        return {setMove, setPlayer};
+    })(boardGrid);
+
+    const player1 = createPlayer("Popo", "$-$");
+    const player2 = createPlayer("Johnson" , "UwU");
+
+    const gameManager = (function (board, controller, players) {
+        controller.setPlayer(players.player1);
+        console.log(players.player1);
+        for (let i = 0; i < 9; i++) {1
+            let x = +prompt("Column");
+            let y = +prompt("Row");
+            controller.setMove(x, y);
+            board.showGrid();
+        }
+
+        return {board, controller};
+    })(boardGrid, gameController, {player1, player2});
+
+    console.log(gameManager.board.showGrid());
     console.log("main ends");
-
-    function Cell () {
-        this.d
-    }
 }
